c2bd16cc6ed5fd053762a4bf33fe33e4e4e9068e
Sverre Rynning-Tønnesen <sverrert@dhcp-10-24-19-45.wlan.ntnu.no>
2022-12-13 11:01:41 +0100

    commented out more

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,19 +1,19 @@
 import client from '../sanity/sanityClient';
 
-/** @type {import('./$types').LayoutLoad} */
-export async function load() {
-	const data = await client.fetch(`*[_type == "bitmojis"]`);
+// /** @type {import('./$types').LayoutLoad} */
+// export async function load() {
+// 	const data = await client.fetch(`*[_type == "bitmojis"]`);
 
-	if (data) {
-		return {
-			status: 200,
-			body: {
-				bitmojis: data
-			}
-		};
-	}
-	return {
-		status: 500,
-		body: new Error('Internal Server Error')
-	};
-}
+// 	if (data) {
+// 		return {
+// 			status: 200,
+// 			body: {
+// 				bitmojis: data
+// 			}
+// 		};
+// 	}
+// 	return {
+// 		status: 500,
+// 		body: new Error('Internal Server Error')
+// 	};
+// }
