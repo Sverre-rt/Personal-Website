83822730a65347c401ea29ace6b48333a869bf58
Sverre Rynning-Tønnesen <sverrert@Sverres-MacBook.local>
2022-12-20 22:12:11 +0100

    experience page made responsive

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -15,8 +15,8 @@
 	export let companies: CompanyType[] = data.body.companies;
 </script>
 
-<div class="flex text-black pt-6 bg-grey-background-1 h-screen">
-	<div class="flex flex-col w-2/5 px-2">
+<div class="flex flex-col md:flex-row text-black pt-6 bg-grey-background-1 h-screen">
+	<div class="flex flex-col px-2 md:w-2/5">
 		<p class="text-4xl mb-4">Technologies worked with</p>
 		<div class="flex flex-col flex-wrap">
 			{#each iterable_technologies as [key, values]}
@@ -37,13 +37,12 @@
 			{/each}
 		</div>
 	</div>
-	<div class="flex flex-col w-3/5 ml-auto">
+	<div class="flex flex-col md:w-3/5 ml-2 md:ml-0">
 		<p class="text-4xl mb-4">My work experience</p>
-		<!-- <div class="flex flex-row flex-wrap gap-6 justify-center"> -->
-		<div class="flex flex-row  gap-4 overflow-x-scroll snap-x scroll-smooth mr-2 scroll-pl-2">
+		<div class="small_screen_experience medium_screen_experience">
 			{#each companies as companie, i}
 				<div
-					class="flex flex-col custom_width bg-grey-background-2 items-center drop-shadow-lg border rounded-lg border-grey-border flex-none snap-start"
+					class="flex flex-col small_width medium_width bg-grey-background-2 items-center drop-shadow-lg border rounded-lg border-grey-border flex-none snap-start my-2"
 				>
 					<div
 						class="flex w-full rounded-t-lg justify-center pt-4 pb-4 border-b-2 border-grey-border {card_bg_colors[
@@ -57,7 +56,7 @@
 							loading="lazy"
 						/>
 					</div>
-					<div class="flex flex-col w-full pr-2 pl-2 ">
+					<div class="flex flex-col w-full px-2">
 						<div class="flex flex-row justify-between items-center">
 							<p class="underline underline-offset-2 font-futuraBold text-lg self-end">
 								{companie.name}
@@ -81,7 +80,7 @@
 							</div>
 							<img
 								src={urlFor(companie.memoryImage).width(1000).url()}
-								class="w-60"
+								class="w-2/5 md:w-60"
 								alt="memory-{companie.name}"
 								loading="lazy"
 							/>
@@ -94,7 +93,34 @@
 </div>
 
 <style>
-	.custom_width {
-		width: 32rem;
+	@media screen and (min-width: 768px) {
+		.medium_width {
+			width: 32rem;
+		}
+
+		.medium_screen_experience {
+			/* md (768px)	 */
+			display: flex;
+			flex-direction: row;
+			gap: 1rem;
+			overflow-x: scroll;
+			scroll-snap-type: x var(--tw-scroll-snap-strictness);
+			scroll-behavior: smooth;
+			margin-right: 0.5rem;
+			scroll-padding-left: 0.5rem;
+		}
+	}
+
+	@media screen and (max-width: 767px) {
+		.small_width {
+			max-width: 32rem;
+		}
+
+		.small_screen_experience {
+			/* sm (640px)	 */
+			display: flex;
+			flex-direction: column;
+			margin-right: 0.5rem;
+		}
 	}
 </style>
