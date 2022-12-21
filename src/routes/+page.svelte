25076bb5f3b12bbaee102aa8e3e7e9ea073c6662
Sverre Rynning-Tønnesen <sverrert98@gmail.com>
2022-12-21 22:29:03 +0100

    improved homepage dynamic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,9 @@
 </script> -->
 <script lang="ts">
 	import type { Social_media } from 'src/types/sanity_types';
-	import { urlFor } from './helper_functions/SanityHelper.svelte';
+	import { asyncUrlFor, urlFor } from './helper_functions/SanityHelper.svelte';
 	import { fade } from 'svelte/transition';
+	import { Spinner } from 'flowbite-svelte';
 
 	let visibility = 'invisible';
 	function handleMouseOver(event: any) {
@@ -20,16 +21,16 @@
 
 <div class="bg-grey-background-1 h-screen flex justify-center items-start">
 	<div
-		class="flex flex-col md:flex-row items-center justify-center md:space-x-16 pt-2 md:pt-12 max-w-3xl"
+		class="flex flex-col md:flex-row items-center justify-center md:space-x-16 pt-2 md:pt-12 w-3xl px-2"
 	>
-		<div class="max-w-2xl px-2 md:px-0 pb-4 md:pb-0">
+		<div class="max-w-xl px-2 md:px-0 pb-4 md:pb-0">
 			<p class="text-3xl">Hi. I'm</p>
 			<p class="text-4xl text-red-home">Sverre Rynning-Tønnesen</p>
 			<div class="p-0.5 {visibility} rounded bg-red-home mt-1 animate-pulse" />
-			<p class="text-xk">
+			<p class="md:text-xl">
 				I’m not a great programmer; I’m just a good programmer with great habits. <br /><br />
 			</p>
-			<p class="text-xl">Welcome to my website and have a look around!</p>
+			<p class="text-xl md:pt-8">Welcome to my website and have a look around!</p>
 			<div class="p-0.5 rounded bg-black mt-2" />
 			<div class="flex flex-row space-x-1 pt-2">
 				{#each social_media_data as s_media}
@@ -51,13 +52,21 @@
 			on:mouseout={handleMouseOut}
 			on:blur={() => void 0}
 		>
-			<img
-				in:fade={{ duration: 1500 }}
-				src={urlFor(data.body.people[0].profile_picture).width(2000).height(2000).url()}
-				class="rounded-full border-8 w-80 drop-shadow-xl "
-				alt="profile_picture"
-				loading="lazy"
-			/>
+			{#await asyncUrlFor(data.body.people[0].profile_picture)}
+				<div
+					class="flex rounded-full border-8 w-64 h-64 drop-shadow-xl justify-center items-center"
+				>
+					<Spinner color="gray" />
+				</div>
+			{:then fetched_url}
+				<img
+					in:fade={{ duration: 1500 }}
+					src={fetched_url.width(2000).height(2000).url()}
+					class="rounded-full border-8 w-64 drop-shadow-xl"
+					alt="profile_picture"
+					loading="lazy"
+				/>
+			{/await}
 		</div>
 	</div>
 </div>
